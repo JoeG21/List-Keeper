eef480a93f108448396122ff5d168965c4d7c506
JoeG21 <jcggomez@live.com>
2021-04-05 23:42:01 -0500

    controllers and models

--- backend/db/migrate/20210317162621_create_notes.rb
+++ backend/db/migrate/20210317162621_create_notes.rb
@@ -1,7 +1,9 @@
 class CreateNotes < ActiveRecord::Migration[6.0]
   def change
     create_table :notes do |t|
-
+      t.integer :user_id
+      t.string :title
+      t.string :body
       t.timestamps
     end
   end
